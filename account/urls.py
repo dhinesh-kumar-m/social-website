175b40b7ac559f4f90bdc399a41d8de5a31f12ac
Dhinesh <dinesh.k131997@gmail.com>
2022-06-09 13:08:19 +0530

    Add registration

--- account/urls.py
+++ account/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
     # path('login/',views.user_login, name ='login'),
     # path('login/', auth_views.LoginView.as_view(), name='login'),
     # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
