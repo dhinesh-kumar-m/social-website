9b79f0b2695b7f3d1657fb5296354abe2485ccaf
Dhinesh <dinesh.k131997@gmail.com>
2022-06-10 11:11:01 +0530

    Add twitter and google auth

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -152,9 +152,15 @@ AUTHENTICATION_BACKENDS = [
  'django.contrib.auth.backends.ModelBackend',
  'account.authentication.EmailAuthBackend',
  'social_core.backends.facebook.FacebookOAuth2',
+ 'social_core.backends.twitter.TwitterOAuth',
+ 'social_core.backends.google.GoogleOAuth2',
+ 
 ]
 
 SOCIAL_AUTH_FACEBOOK_KEY =  env('SOCIAL_AUTH_FACEBOOK_KEY') # Facebook App ID
 SOCIAL_AUTH_FACEBOOK_SECRET =  env('SOCIAL_AUTH_FACEBOOK_SECRET') # Facebook App Secret
-
+SOCIAL_AUTH_TWITTER_KEY = env('SOCIAL_AUTH_TWITTER_KEY')
+SOCIAL_AUTH_TWITTER_SECRET = env('SOCIAL_AUTH_TWITTER_SECRET')
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = env('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = env('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')
 SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
