53270595e25ee3e0bd8f377da5da4440f25b8234
Dhinesh <dinesh.k131997@gmail.com>
2022-06-13 11:45:09 +0530

    Add Detail view for image

--- images/views.py
+++ images/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 
 from images.models import Image
 from .forms import ImageCreateForm
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 @login_required
@@ -24,3 +25,7 @@ def image_create(request):
         form = ImageCreateForm(data=request.GET)
     
     return render(request,'images/image/create.html', {'section': 'images','form': form})
+
+def image_detail(request, id , slug):
+    image = get_object_or_404(Image, id=id, slug = slug)
+    return render(request,'images/image/detail.html',{"section":'images',"image": image})
