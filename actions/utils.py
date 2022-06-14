785a1dda4c3058605760353dc82aaa5db51ced20
Dhinesh <dinesh.k131997@gmail.com>
2022-06-14 17:19:44 +0530

    Fix user like bug

--- actions/utils.py
+++ actions/utils.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 def create_action(user, verb, target=None):
     now = timezone.now()
-    last_minute = now - datetime.timedelta(sectonds=60)
+    last_minute = now - datetime.timedelta(seconds=60)
     similar_actions = Action.objects.filter(user_id=user.id,verb=verb,created__gte= last_minute)
 
     if target:
