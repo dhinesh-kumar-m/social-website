d5dba522d749aa782a1f0d20b039c100540c340f
Dhinesh <dinesh.k131997@gmail.com>
2022-06-14 17:01:13 +0530

    Add prefetch_related() in action list

--- account/views.py
+++ account/views.py
@@ -42,8 +42,8 @@ def dashboard(request):
     following_ids = request.user.following.values_list('id',flat=True)
 
     if following_ids:
-        actions = actions.filter(user_id__in=following_ids)[:10]
-        actions = actions.select_related('user','user__profile')
+        actions = actions.filter(user_id__in=following_ids)
+        actions = actions.select_related('user','user__profile').prefetch_related('target')[:10]
     return render(request,'account/dashboard.html',{'section': 'dashboard','actions':actions})
     
 def register(request):
