05cbd89cb41275674035c3de7d25b9adc11fe10d
root <root@Testpress.localdomain>
2022-06-14 19:29:53 +0530

    Add redis view count method

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -173,3 +173,8 @@ SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
 ABSOLUTE_URL_OVERRIDES = {
     'auth.user': lambda u: reverse_lazy('user_detail',args=[u.username])
 }
+
+# Redis Configurations
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
--- images/views.py
+++ images/views.py
@@ -13,6 +13,11 @@ from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
 from common.ajax import is_ajax
 from actions.utils import create_action
+import redis
+from django.conf import settings
+
+#connect to redis
+r = redis.Redis(host=settings.REDIS_HOST,port=settings.REDIS_PORT,db=settings.REDIS_DB)
 # Create your views here.
 @login_required
 def image_create(request):
@@ -36,7 +41,10 @@ def image_create(request):
 
 def image_detail(request, id , slug):
     image = get_object_or_404(Image, id=id, slug = slug)
-    return render(request,'images/image/detail.html',{"section":'images',"image": image})
+    # increment total image views by 1
+    # You build the Redis key using a notation, such as object-type:id:field (for example, image:33:id).
+    total_views=r.incr(f'image:{image.id}:views')
+    return render(request,'images/image/detail.html',{"section":'images',"image": image,'total_views': total_views})
 
 @ajax_required
 @login_required
